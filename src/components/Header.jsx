bfefa6aeedd5305bc7a2f2d49662d0539a627451
Timur <timur.mosenkov@gmail.com>
2024-11-24 14:16:36 +0200

    add header logo section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,43 @@
 import { useState } from "react";
 import burgerButton from "../assets/icons/burger.png";
+import searchLogo from "../assets/icons/search.png";
 import logoImg from "../assets/img/SHOP.CO.png";
 import closeImg from "../assets/icons/close.png";
+import cartLogo from '../assets/icons/cart.png'
+import profileLogo from '../assets/icons/profile.png'
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       {
-        <div className="flex items-center gap-4">
-          {!isOpen && (
-            <img
-              className="z-40 cursor-pointer relative w-6 h-6"
-              src={burgerButton}
-              alt="Burger Menu"
-              onClick={() => setIsOpen(!isOpen)}
-            />
-          )}
-          {isOpen && (
-            <img
-              src={closeImg}
-              alt=""
-              className="z-30 relative w-6 text"
-              onClick={() => setIsOpen(!isOpen)}
-            />
-          )}
-          <a href="">
-            <img src={logoImg} alt="" className="" />
-          </a>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-4">
+            {!isOpen && (
+              <img
+                className="z-40 cursor-pointer relative w-6 h-6"
+                src={burgerButton}
+                alt="Burger Menu"
+                onClick={() => setIsOpen(!isOpen)}
+              />
+            )}
+            {isOpen && (
+              <img
+                src={closeImg}
+                alt=""
+                className="z-30 relative w-6 text"
+                onClick={() => setIsOpen(!isOpen)}
+              />
+            )}
+            <a href="">
+              <img src={logoImg} alt="" className="" />
+            </a>
+          </div>
+          <div className='flex  gap-4'>
+            <img src={searchLogo} alt="" />
+            <img src={cartLogo} alt="" />
+            <img src={profileLogo} alt="" />
+          </div>
         </div>
       }
 
@@ -37,7 +47,7 @@ const Header = () => {
         } z-20`}
       >
         <nav className="flex justify-center items-center flex-col h-full z-10 text-sm">
-          <ul className="font-bold space-y-4">
+          <ul className="font-bold space-y-7">
             <li className="px-2 py-1 bg-emerald-300 flex justify-center rounded-sm">
               <a href="">Shop</a>
             </li>
