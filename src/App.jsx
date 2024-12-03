fafd7dd24985a07e1e01172a86049868c2fa7163
Timur <timur.mosenkov@gmail.com>
2024-12-03 20:36:46 +0200

    create new arriavals setion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import Header from './components/Header'
 import MainSection from './components/MainSection'
+import NewArrivals from './components/NewArrivals'
 
 const App = () => {
   return (
     <div className='container max-w-custom-container mx-auto'>
       <Header />
       <MainSection />
+      <NewArrivals />
     </div>
   )
 }
