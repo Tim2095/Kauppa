e0379d5dfb6e50e26c889e3d559c8fdb2cede13e
Timur <timur.mosenkov@gmail.com>
2025-01-01 17:04:04 +0200

    add new styles section

---
+++ src/components/DressStyle.jsx
@@ -0,0 +1,16 @@
+
+const DressStyle = () => {
+  return (
+    <div className="bg-[#F0F0F0] h-screen mt-12 ml-4 mr-4">
+      <h2 className="text-center">BROWSE BY dress STYLE</h2>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default DressStyle
