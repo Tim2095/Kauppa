a3e77c485bf653a039ca67d35a33a129a8e20cd7
Timur <timur.mosenkov@gmail.com>
2025-01-01 17:31:07 +0200

    ready styles section cpmonent

--- src/components/DressStyle.jsx
+++ src/components/DressStyle.jsx
@@ -1,13 +1,18 @@
-
+import casualImg from '../assets/img/selection/mobile/casual.png'
+import formalImg from '../assets/img/selection/mobile/formal.png'
+import partyImg from '../assets/img/selection/mobile/party.png'
+import gymImg from '../assets/img/selection/mobile/gym.png'
 const DressStyle = () => {
   return (
-    <div className="bg-[#F0F0F0] h-screen mt-12 ml-4 mr-4">
-      <h2 className="text-center">BROWSE BY dress STYLE</h2>
-      <div>
-
+    <div className="bg-[#F0F0F0] mt-12 ml-4 mr-4 rounded-lg flex justify-center items-center flex-col gap-y-4 pb-6">
+      <h2 className="font-bold font-anton text-center pt-10 text-3xl max-w-60 leading-9">BROWSE BY DRESS STYLE</h2>
+      <div className='flex flex-col gap-y-4'>
+        <img src={casualImg} alt="" />
+        <img src={formalImg} alt="" />
       </div>
-      <div>
-
+      <div className='flex flex-col gap-y-4'>
+        <img src={partyImg} alt="" />
+        <img src={gymImg} alt="" />
       </div>
     </div>
   )
