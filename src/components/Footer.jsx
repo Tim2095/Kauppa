70b3e5921052804dcf2fc7190bc944723ce51606
Timur <timur.mosenkov@gmail.com>
2025-01-09 21:54:17 +0200

    import social media logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import twitterLogo from '../assets/logo/twitter.png'
+import instagramLogo from '../assets/logo/instagram.png'
+import facebookLogo from '../assets/logo/facebook.png'
+import githubLogo from '../assets/logo/github.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div>
+        <h3>SHOP.CO</h3>
+        <p>We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
+      </div>
+
+    </footer>
   )
 }
 
