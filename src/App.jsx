13311f42636a895689ec38b8e8f8806846401b65
Timur <timur.mosenkov@gmail.com>
2025-01-19 19:27:10 +0200

    provide reducer for all app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useDispatch } from "react-redux";
+import { getInitialProducts } from "./reducer/product-slice";
+import productServices from './services/product'
 import CustomerReview from "./components/CustomerReview";
 import DressStyle from "./components/DressStyle";
 import Footer from "./components/Footer";
@@ -6,10 +8,15 @@ import Header from "./components/Header";
 import MainSection from "./components/MainSection";
 import NewArrivals from "./components/NewArrivals";
 import SubscribeNews from "./components/SubscribeNews";
+import { useEffect } from "react";
 
 const App = () => {
   const dispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(productServices.getAllProducts)
+  })
+ 
   return (
     <div className="container max-w-custom-container mx-auto">
       <Header />
