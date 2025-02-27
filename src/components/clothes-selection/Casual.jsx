cfd6b91b18ecf25b1ab5142c23b433edda9b67cd
Timur <timur.mosenkov@gmail.com>
2025-02-27 21:52:24 +0200

    adjust casual section

--- src/components/clothes-selection/Casual.jsx
+++ src/components/clothes-selection/Casual.jsx
@@ -1,18 +1,15 @@
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 
 const Casual = () => {
   const products = useSelector((state) => state.product);
 
   return (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
       {products.map((product) => (
-        <div key={product.id}>
-          <div>
-            <img src={product.image} alt="" />
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-          </div>
+        <div key={product.id} className="bg-white p-4 rounded-lg shadow-md">
+          <img src={product.image} alt={product.name} className="w-full h-40 sm:h-48 md:h-64 object-cover rounded-lg" />
+          <p className="mt-2 text-lg font-semibold">{product.name}</p>
+          <p className="text-gray-600">${product.price}</p>
         </div>
       ))}
     </div>
