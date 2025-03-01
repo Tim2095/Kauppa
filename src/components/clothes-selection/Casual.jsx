babdeb4bfc2ca4d4a2840d13d01d9f64f3848bc5
Timur <timur.mosenkov@gmail.com>
2025-03-01 16:58:51 +0200

    add link to each product

--- src/components/clothes-selection/Casual.jsx
+++ src/components/clothes-selection/Casual.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Casual = () => {
   const products = useSelector((state) => state.product);
@@ -6,11 +7,17 @@ const Casual = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
       {products.map((product) => (
-        <div key={product.id} className="bg-white p-4 rounded-lg shadow-md">
-          <img src={product.image} alt={product.name} className="w-full h-40 sm:h-48 md:h-64 object-cover rounded-lg" />
-          <p className="mt-2 text-lg font-semibold">{product.name}</p>
-          <p className="text-gray-600">${product.price}</p>
-        </div>
+        <Link to={`/products/${product.id}`} key={product.id}>
+          <div key={product.id} className="bg-white p-4 rounded-lg shadow-md">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="w-full h-40 sm:h-48 md:h-64 object-cover rounded-lg"
+            />
+            <p className="mt-2 text-lg font-semibold">{product.name}</p>
+            <p className="text-gray-600">${product.price}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
