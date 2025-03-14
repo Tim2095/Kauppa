0ebc483ac34a5cf3af28929ad60f8bf25038bec3
Timur <timur.mosenkov@gmail.com>
2025-03-14 21:46:47 +0200

    create cart product slice

--- src/reducer/cart-slice.js
+++ src/reducer/cart-slice.js
@@ -1,2 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  cartProducts: []
+}
+
+const productSlice = createSlice({
+  name: "cartProducts",
+  initialState: initialState,
+  reducers: {
+    addProduct(state, action) {
+      state.cartProducts.push(action.payload)
+    },
+  },
+});
+
+export const { addProduct } = productSlice.actions;
+export default productSlice.reducer;
