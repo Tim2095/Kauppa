2f3584529923cbb148407a47901efb06e8bfd1a0
Timur <timur.mosenkov@gmail.com>
2025-03-27 20:21:30 +0200

    style cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
+import deleteLogo from "../assets/logo/mobile/delete-logo.png";
 
 const Cart = () => {
   const products = useSelector((state) => state.cart.cartProducts);
@@ -13,7 +14,7 @@ const Cart = () => {
       <span key={routeTo}>
         <Link
           to={routeTo}
-          className=" hover:underline capitalize font-dmSans text-sm"
+          className="hover:underline capitalize font-dmSans text-sm"
         >
           {decodeURIComponent(path.replace(/-/g, " "))}
         </Link>
@@ -25,38 +26,42 @@ const Cart = () => {
   return (
     <div className="p-4">
       <nav className="mb-4 text-gray-600">
-        <Link to="/" className=" hover:underline font-dmSans text-sm">
+        <Link to="/" className="hover:underline font-dmSans text-sm">
           Home
         </Link>
         {paths.length > 0 && " > "}
         {breadcrumbLinks}
-        <div className="flex justify-between items-center">
-          <h2 className="mt-3 font-dmSans bold text-2xl font-bold">
-            Your Cart
-          </h2>
-        </div>
       </nav>
 
-      <div className="border rounded-xl px-3 py-2">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="font-dmSans text-2xl font-bold">Your Cart</h2>
+      </div>
+
+      <div className="border rounded-xl px-3 py-1">
         {products.map((product) => (
-          <div key={product.id} className="flex border-b py-4">
-            <div className="mr-3">
+          <div
+            key={product.id}
+            className="flex border-b py-3  justify-between"
+          >
+            <div className="flex">
               <img
                 src={product.img}
                 alt="added product img"
-                className="w-[100px] h-[120px]"
+                className="w-[100px] h-[120px] mr-3"
               />
-            </div>
-            <div>
-              <div>
-                <p>{product.name}</p>
-                <p>Color: {product.color}</p>
-                <p>Size: {product.size}</p>
+              <div className="flex flex-col">
+                <p className="font-dmSans text-base font-bold min-w-[100px]">
+                  {product.name}
+                </p>
+                <div className="flex text-sm flex-col text-gray-600">
+                  <p>Color: {product.color}</p>
+                  <p>Size: {product.size}</p>
+                </div>
               </div>
-              <div>
-
-              </div>
             </div>
+            <button className="w-6 h-6 flex items-center justify-center">
+              <img src={deleteLogo} alt="Delete" className="w-full h-full" />
+            </button>
           </div>
         ))}
       </div>
